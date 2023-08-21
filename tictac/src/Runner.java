f6357396d40bb95276d63029ab51b4624920a524
Sean Caspy <101971943+SeanCaspy@users.noreply.github.com>
2023-08-21 23:42:05 +0300

    Update Runner.java

--- tictac/src/Runner.java
+++ tictac/src/Runner.java
@@ -2,12 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         Game g = new Game();
         g.runGame();
-//        Game[] r = new Game[100];
-//        for (int i = 0; i < 100; i++) {
-//            r[i] = new Game();
-//            System.out.println(r[i].turn);
-//        }
-
 
     }
 }
